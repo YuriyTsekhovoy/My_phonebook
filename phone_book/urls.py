c7fb7b7a0b3e27ffc6c215c8c16a18613ed0c965
YuriyTsekhovoy <yuriy.tsekhovoy@gmail.com>
2017-09-12 09:01:25 +0300

    a branch with BaseViews

--- phone_book/urls.py
+++ phone_book/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from . import views
 
-#app_name = "phone_book"
+app_name = "phone_book"
 urlpatterns = [
-    url(r'^$', views.sign_list, name='sign_list'),
-    url(r'^sign/(?P<pk>[0-9]+)/$', views.sign_detail, name='sign_detail'),
-    url(r'^sign/new/$', views.sign_new, name='sign_new'),
-    url(r'^sign/(?P<pk>[0-9]+)/edit/$', views.sign_edit, name='sign_edit'),
-    url(r'^sign/(?P<pk>[0-9]+)/delete/$', views.sign_delete, name='sign_delete'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^sign/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^edit$', views.NewSignView.as_view(), name='sign_new'),
+    url(r'^sign/(?P<pk>[0-9]+)/edit/$', views.EditNumberView.as_view(), name='edit'),
+    url(r'^sign/(?P<pk>[0-9]+)/delete/$', views.DelView.as_view(), name='delete'),
     #url(r'^sign/(?P<pk>[0-9]+)/copy/$', views.sign_copy, name='sign_copy'),
 ]
--- phone_book/views.py
+++ phone_book/views.py
@@ -1,56 +1,41 @@
-from django.shortcuts import render, get_list_or_404
-from django.utils import timezone
-from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.views import generic
+from django.conf.urls import url
+
 from .models import SignName, PhoneNumber
 from .forms import NumberForm, SignForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 
-def sign_list(request):
-    signs = SignName.objects.all()
-    return render(request, 'phone_book/sign_list.html', {'signs': signs})
+class IndexView(generic.ListView):
+    model = SignName
+    template_name = 'phone_book/index.html'
+    context_object_name = 'sign_list'
 
 
-def sign_detail(request, pk):
-    name = SignName.objects.get(id=pk)
-    sign = PhoneNumber.objects.filter(name_id=pk)
-    return render(request, 'phone_book/sign_detail.html', {'sign': sign, 'name': name})
+class DetailView(generic.ListView):
+    model = PhoneNumber
+    template_name = 'phone_book/detail.html'
+    context_object_name = 'numbers'
+    #queryset = PhoneNumber.objects.all()
+
+    def get_queryset(self):
+        return PhoneNumber.objects.filter(name_id=self.kwargs['pk'])
 
 
-def sign_new(request):
-    if request.method == 'POST':
-        form = SignForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.save()
-            return redirect('sign_detail', pk=post.pk)
-    else:
-        form = SignForm()
-    return render(request, 'phone_book/sign_edit.html', {'form': form})
+class EditNumberView(generic.CreateView):
+    model = PhoneNumber
+    fields = ['number', 'is_mobile']
+    template_name = 'phone_book/edit.html'
+    success_url = '/'
 
+class DelView(generic.DeleteView):
+    model = SignName
+    success_url = '/'
 
-def sign_edit(request, pk):
-    if request.method == "POST":
-        form = NumberForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.name = SignName.objects.get(id=pk)
-            post.save()
-            return redirect('sign_detail', pk)
-    else:
-        form = NumberForm()
-    return render(request, 'phone_book/number_edit.html', {'form': form})
-
-
-def sign_delete(request, pk):
-    sign = SignName.objects.get(id=pk)
-    sign.delete()
-    return redirect('sign_list')
-
-'''
-def sign_copy(request, pk):
-    sign = SignName.objects.get(id=pk)
-    sign.save()
-    return redirect('sign_list')
-'''
+class NewSignView(generic.CreateView):
+	model = SignName
+	fields = ['sign_name']
+	success_url = '/'
